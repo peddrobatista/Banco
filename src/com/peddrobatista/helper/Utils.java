6a4809c756854d7eb5e2ee8a43790079f9859791
pedro-batista <peddrobatista2022@gmail.com>
2023-11-17 15:22:51 -0300

    My First Commit

---
+++ src/com/peddrobatista/helper/Utils.java
@@ -0,0 +1,36 @@
+package com.peddrobatista.helper;
+
+import java.text.*;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class Utils {
+    static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    static NumberFormat nf = new DecimalFormat("R$ #,##0.00",
+            new DecimalFormatSymbols(new Locale("pt", "BR")));
+
+    public static String dateParaString(Date data) {
+        return Utils.sdf.format(data);
+    }
+
+    public static String doubleParaString(Double valor) {
+        return Utils.nf.format(valor);
+    }
+
+    public static Double stringParaDouble(String valor) {
+        try {
+            return (Double)Utils.nf.parse(valor);
+        }catch(ParseException e) {
+            return null;
+        }
+    }
+
+    public static void pausar(int segundos) {
+        try {
+            TimeUnit.SECONDS.sleep(segundos);
+        }catch(InterruptedException e) {
+            System.out.println("Erro ao pausar por " + segundos + " segundos.");
+        }
+    }
+}
